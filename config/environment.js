d2227d3efc822decebed8fc3ebfa590b38fe8073
Edward Gillard <edwardgillard@edwards-mbp.mynet>
2020-06-16 17:20:18 +0100

    update

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
-const port = 8000
-const dbURI = 'mongodb://localhost/bangers-playlist'
-const secret = 'mobgoeswild'
+const port = process.env.PORT || 8000
+const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/bangers-playlist'
+const secret = process.env.SECRET || 'mobgoeswild'
 
 module.exports = {
   dbURI,
